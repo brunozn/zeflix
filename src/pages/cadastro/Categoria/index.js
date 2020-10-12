d59faa2813def9bad7b66c1466155faee671a939
brunozn <brunojndias@gmail.com>
2020-10-12 10:08:28 -0300

    att

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,23 +1,93 @@
-import React from "react";
-import PageDefault from "../../../components/PageDefault";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import PageDefault from '../../../components/PageDefault';
+import { Link } from 'react-router-dom';
 
 // import { Container } from './styles';
 
 function CadastroCategoria() {
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '',
+  };
+  const [categorias, setCategorias] = useState([]);
+  const [values, setValues] = useState(valoresIniciais);
+
+  function setValue(chave, valor) {
+    // chave: nome, descricao, bla, bli
+    setValues({
+      ...values,
+      [chave]: valor, // nome: 'valor'
+    });
+  }
+
+  function handleChange(infosDoEvento) {
+    setValue(
+      infosDoEvento.target.getAttribute('name'),
+      infosDoEvento.target.value
+    );
+  }
+
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria</h1>
+      <h1>Cadastro de Categoria: {values.nome}</h1>
 
-      <form>
-        <label>
-          Nome da Categoria:
-          <input type="text" />
-        </label>
+      <form
+        onSubmit={function handlerSubmit(InfosEvent) {
+          InfosEvent.preventDefault();
+          setCategorias([...categorias]);
+          console.log('form enviando...');
+        }}
+      >
+        <div>
+          <div>
+            <label>
+              Nome da Categoria:
+              <input
+                type="text"
+                value={values.nome}
+                onChange={function functionHandler(infos) {
+                  setValue(infos.target.value);
+                }}
+              />
+            </label>
+          </div>
+
+          <div>
+            <label>
+              Descrição:
+              <textarea
+                type="text"
+                value={values.descricao}
+                onChange={function functionHandler(infos) {
+                  setValues(infos.target.value);
+                }}
+              />
+            </label>
+          </div>
+
+          <div>
+            <label>
+              Cor
+              <input
+                type="color"
+                value={values.cor}
+                onChange={function functionHandler(infos) {
+                  setValues(infos.target.value);
+                }}
+              />
+            </label>
+          </div>
+        </div>
 
         <button>Cadastrar</button>
       </form>
 
+      <ul>
+        {categorias.map((categoria, indice) => {
+          return <li key={`${categoria}${indice}`}>{categoria}</li>;
+        })}
+      </ul>
       <Link to="/">Voltar para Home</Link>
     </PageDefault>
   );
